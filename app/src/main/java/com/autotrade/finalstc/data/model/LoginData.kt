5996906879dbd07ddd67eb1be3f49c038ba08d30
aryasis87 <aryasis87@gmail.com>
2025-11-04 15:01:36 +0700

    Apply .gitignore to remove data folder from Git tracking

--- app/src/main/java/com/autotrade/finalstc/data/model/LoginData.kt
+++
@@ -1,9 +0,0 @@
-package com.autotrade.finalstc.data.model
-
-import com.google.gson.annotations.SerializedName
-
-data class LoginData(
-    val authtoken: String,
-    @SerializedName("user_id")
-    val userId: String
-)
--- app/src/main/java/com/autotrade/finalstc/data/model/LoginRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.autotrade.finalstc.data.model
-
-import com.google.gson.annotations.SerializedName
-
-data class LoginRequest(
-    val email: String,
-    val password: String
-)
